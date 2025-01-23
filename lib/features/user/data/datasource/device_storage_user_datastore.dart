b8e0eaf51efe4b46bcf71be1150c1874a60aa6be
Samuel Ramos <114710082+Tonalga69@users.noreply.github.com>
2025-01-23 16:41:46 -0600

    profile photo wasn´t saved correctly
    bug fixed

--- lib/features/user/data/datasource/device_storage_user_datastore.dart
+++ lib/features/user/data/datasource/device_storage_user_datastore.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:appointments_manager/core/services/object_box_service.dart';
 import 'package:get/get.dart';
 import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 
 class DeviceStorageUserDatastore extends GetxService {
   static DeviceStorageUserDatastore get to =>
@@ -15,10 +16,20 @@ class DeviceStorageUserDatastore extends GetxService {
 
   Future<String> saveProfileImage(String imagePath) async {
     final folder = _objectBoxService.profileFolder;
-    final profileImage = p.join(folder, profileImageFolder);
+    final docsDir = await getApplicationDocumentsDirectory();
+    final profileImagePath = p.join(docsDir.path, folder, profileImageFolder);
+    // Create the directory if it doesn't exist
+    await _createDirectory(profileImagePath);
     final file = File(imagePath);
     final fileName = p.basename(file.path);
-    final newFile = await file.copy('$profileImage/$fileName');
+    final newFile = await file.copy('$profileImagePath/$fileName');
     return newFile.path;
+  }
+
+  Future<void> _createDirectory(String path) async {
+    final directory = Directory(path);
+    if (!await directory.exists()) {
+      await directory.create(recursive: true);
+    }
   }
 }
